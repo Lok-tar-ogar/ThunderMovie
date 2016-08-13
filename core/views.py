ff5d4be5e0c6328d0f75e910b83a1c70ecb42951
macair <418586403@qq.com>
2016-08-13 11:17:17 +0800

    ss

--- core/views.py
+++ core/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from core.models import *
 from django.http import HttpResponse,HttpResponseRedirect,Http404,HttpResponseNotFound
 import random
+import os
+import json
 def index(req):
     films=FILM.objects.all()[:50]
     return render(req,'index.html',locals())
-
+def gitpull(req):
+    msg = os.popen('sudo sh /home/ubuntu/ThunderMovie/deploy.sh').read()
+    return HttpResponse(json.dumps({'msg:': msg}))
 
 def search(req,keywords=''):
-    films=FILM.objects.filter(film_intro__contains=keywords,film_intro__contains=keywords)
+    films=FILM.objects.filter(film_intro__contains=keywords,film_name__contains=keywords)
     return render(req, 'about.html', locals())
 
 def single(req,fid=0):
