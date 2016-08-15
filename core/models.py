ed135d12b9ac9cc379ad113220d07e112df1988e
macair <418586403@qq.com>
2016-08-15 10:42:14 +0800

    ss

--- core/models.py
+++ core/models.py
@@ -22,6 +22,7 @@ class FILM(models.Model):
     film_actors=models.CharField('演员们',max_length=500,null=True)
     film_disc= models.CharField('电影评论',max_length=5000, null=True)
     film_pub_year=models.CharField('电影发行年代',max_length=50, null=True)
+    film_country=models.CharField('电影来自国家',max_length=50, null=True)
     dim_date=models.DateTimeField(auto_now_add=True,null=True)
     def __str__(self):
         return self.film_name
