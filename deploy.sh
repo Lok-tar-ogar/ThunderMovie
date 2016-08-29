efa0b9786327f2006ffef860290efa633ab7ac52
macair <418586403@qq.com>
2016-08-29 16:22:16 +0800

    ss

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/ThunderMovie
 sudo git pull
-sudo killall uwsgi
-sudo uwsgi /home/ubuntu/ThunderMovie/movie.ini
-sudo /usr/sbin/nginx -s reload
+#sudo killall uwsgi
+#sudo uwsgi /home/ubuntu/ThunderMovie/movie.ini
+#sudo /usr/sbin/nginx -s reload
